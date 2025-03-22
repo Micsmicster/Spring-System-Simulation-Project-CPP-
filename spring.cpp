32c5d7c603039b6508b4a4001017a3693f948017
Micsmicster <mickeykpis@gmail.com>
2025-03-22 15:00:28 -0700

    Create spring.cpp

---
+++ spring.cpp
@@ -0,0 +1,45 @@
+#include "spring_system.h"
+
+// Add Spring
+void SpringSystem::addSpring(double k, double c, double mass) {
+    springs.push_back(Spring(k, c, mass));
+}
+
+// Display All Springs
+void SpringSystem::displayAllSprings() {
+    if (springs.empty()) {
+        cout << "No springs available." << endl;
+        return;
+    }
+    for (size_t i = 0; i < springs.size(); i++) {
+        cout << "Spring " << i + 1 << ":" << endl;
+        springs[i].display();
+        cout << "------------------------" << endl;
+    }
+}
+
+// Simulate Motion
+void SpringSystem::simulate(double timeStep, double totalTime) {
+    cout << "Simulating Spring Motion..." << endl;
+    for (double t = 0; t <= totalTime; t += timeStep) {
+        cout << "Time: " << t << "s" << endl;
+        for (size_t i = 0; i < springs.size(); i++) {
+            double response = springs[i].computeTransferFunction(t);
+            cout << "Spring " << i + 1 << " Response: " << response << endl;
+        }
+        cout << "------------------------" << endl;
+    }
+}
+
+// Save to File
+void SpringSystem::saveToFile(string filename) {
+    ofstream outFile(filename);
+    if (!outFile) {
+        cout << "Error saving file!" << endl;
+        return;
+    }
+    for (Spring& s : springs) {
+        outFile << s.computeTransferFunction(1.0) << endl;  // Save a sample value
+    }
+    cout << "Data saved successfully!" << endl;
+}
